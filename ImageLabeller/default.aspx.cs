01dd50e0e2f18a29b446560a59972d4ff22421e9
meminyldz <m.eminyldz92@gmail.com>
2016-10-12 22:19:59 +0300

    resim büyütme problem solved

--- ImageLabeller/default.aspx.cs
+++ ImageLabeller/default.aspx.cs
@@ -50,7 +50,7 @@ namespace ImageLabeller
         {
             try
             {
-                var mongoClient = new MongoClient(""); // Connection String
+                var mongoClient = new MongoClient("mongodb://omeryasin32:AkifEminOmer34@ds055626.mlab.com:55626/11102016justurlbackup"); // Connection String
                 var server = mongoClient.GetServer();
                 mongoDatabase = server.GetDatabase("11102016justurlbackup"); // Database Name 
                 mongoCollection = mongoDatabase.GetCollection("Url"); // Collection name
@@ -77,7 +77,7 @@ namespace ImageLabeller
 
                 ImgUrl = url.image;
                 ImgID = url._id;
-
+                ImgUrl = ImgUrl.Replace("_normal", "");
                 return ImgUrl;
             }
             catch (Exception)
@@ -93,7 +93,7 @@ namespace ImageLabeller
         {
             try
             {
-                var mongoClient = new MongoClient(""); // Connection String
+                var mongoClient = new MongoClient("mongodb://omeryasin32:AkifEminOmer34@ds019628.mlab.com:19628/gridfs1"); // Connection String
                 var server = mongoClient.GetServer();
                 gridFSDatabase = server.GetDatabase("gridfs1"); // Database Name 
 
